4b89e78710f80b5d9c9db415099d6adae6857990
DiLeyRa <ing.dianaleyva16@gmail.com>
2018-08-29 16:02:39 -0500

    lectura de archivos .md

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+fs.readFile('./README.md', data = (err,data) => {
+    if(err){
+        console.log(err);
+    }
+    text = data.toString();
+    // console.log(text);
+
+    for(let i=0; i<text.lenght; i++){
+        let links = [];
+        
+    }
+})
