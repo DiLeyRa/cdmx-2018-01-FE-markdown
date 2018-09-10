999cd7820a2a72d2f58d35248ccc75d1612d775b
DiLeyRa <ing.dianaleyva16@gmail.com>
2018-09-10 09:36:08 -0500

    corrigiendo identación

--- app.js
+++ app.js
@@ -6,59 +6,58 @@ const array = [];
 const fetch = require('node-fetch');
 
 const readMd = () => {
-    fs.readFile('./README.md', 'utf8', (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            //console.log(data);
-            convertHtml(data);
-        }
-    })
-}
+  fs.readFile('./README.md', 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // console.log(data);
+      convertHtml(data);
+    }
+  });
+};
 readMd();
 
 const convertHtml = (data) => {
-    const tokens = marked.lexer(data);
-    //console.log(tokens);
-    const html = marked.parser(tokens);
-    //console.log(html);
-    extractUrl(html);
-
-}
+  const tokens = marked.lexer(data);
+  // console.log(tokens);
+  const html = marked.parser(tokens);
+  // console.log(html);
+  extractUrl(html);
+};
 const extractUrl = (html) => {
-    linkArray = [];
-    const dom = new JSDOM(html);
-    const etiqueta = dom.window.document.querySelectorAll("a"); 
-    etiqueta.forEach(links = (a) => {
-        let urll = a.href;
-        let txtCont = a.textContent;
-        let linkObj = {
-            href: a.href,
-            text: a.textContent,
-            file: ''
-        };
-        linkArray.push(linkObj);
-        validation(urll, txtCont);
-    });
-    console.log(linkArray);
-    states(linkArray);
-}
+  linkArray = [];
+  const dom = new JSDOM(html);
+  const etiqueta = dom.window.document.querySelectorAll('a'); 
+  etiqueta.forEach(links = (direction) => {
+    let urll = direction.href;
+    let txtCont = direction.textContent;
+    let linkObj = {
+      href: direction.href,
+      text: direction.textContent,
+      file: ''
+    };
+    linkArray.push(linkObj);
+    validation(urll, txtCont);
+  });
+  console.log(linkArray);
+  states(linkArray);
+};
 
 const validation = (urll, txtCont) => {
-    // console.log(urll);
-    fetch(urll)
+  // console.log(urll);
+  fetch(urll)
     .then(function(response) {
-        console.log(response.url, response.statusText, response.status, txtCont);
+      console.log(response.url, response.statusText, response.status, txtCont);
     })
     .catch(function(err) {
-        console.error(err);
+      console.error(err);
     });
-}
+};
 
 const states = (linkArray) =>{
-    console.log('Total :', linkArray.length);
-    let uniqs = linkArray.filter(function (item,index,array){
-        return array.indexOf(item) === index;
-    })
-    console.log('Uniqs :',uniqs.length)
-}
+  console.log('Total :', linkArray.length);
+  let uniqs = linkArray.filter(function(item, index, array) {
+    return array.indexOf(item) === index;
+  });
+  console.log('Uniqs :', uniqs.length);
+};
---
+++ cli.js
@@ -0,0 +1,2 @@
+const [,, ...args] = process.argv;
+console.log(`Hey you ${args}`);
